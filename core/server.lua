bede2b51a3f3264bf6d1dc07dc769b4351739fd2
Nutto.jsx <91049342+Nutmito@users.noreply.github.com>
2024-05-06 05:59:46 +0700

    ASDS

--- core/server.lua
+++ core/server.lua
@@ -1,6 +1,6 @@
 RegisterServerEvent('nt_core:ResourceStatus')
 AddEventHandler('nt_core:ResourceStatus', function(resource)
     if resource == GetCurrentResourceName() then
-        print("^7[^2" .. GetCurrentResourceName() .. "^7] Status ^5200 ^7OK")
+        print("^7[^2SCRIPT^7] ".. GetCurrentResourceName() .." Status ^5200 ^7OK")
     end
 end)
