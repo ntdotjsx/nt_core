79f8873a4480b44c6c9f2b96269e26f56a1daf71
Nutto.jsx <91049342+Nutmito@users.noreply.github.com>
2024-05-07 20:52:10 +0700

    New Core

---
+++ modules/ntwep/config.lua
@@ -0,0 +1,144 @@
+-- @ ตั้งดาเมจตัวเกม
+Config.DamageWeapon = {
+    {
+		Name = `WEAPON_BOTTLELV1`,     -- ชื่ออาวุธ
+		-- Damage = 0.15,              -- ความแรง
+		Damage = 0.90,                 -- ความแรง
+        ArmourDamage = 15,             -- ดาเมจตายตัว เช่น 12 ก็คือ 12
+        Critral = 0,                   -- % คริ
+        Painkiller = {
+            Percent = 0,               -- โอกาสติดห้ามปั้มยา
+            TimeOut = 0,               -- สถานะคงอยู่กี่วินาที
+        },
+        Piercing = {
+            Percent = 0,               -- โอกาสทะลุ
+            Damage_Percent = 0         -- ดาเมจที่จะทะลุ 
+        }
+	},
+    {
+		Name = `WEAPON_BOTTLELV2`,     -- ชื่ออาวุธ
+		Damage = 0.90,                 -- ความแรง
+        ArmourDamage = 20,             -- ดาเมจตายตัว เช่น 12 ก็คือ 12
+        Critral = 0,                   -- % คริ
+        Painkiller = {
+            Percent = 0,               -- โอกาสติดห้ามปั้มยา
+            TimeOut = 0,               -- สถานะคงอยู่กี่วินาที
+        },
+        Piercing = {
+            Percent = 0,               -- โอกาสทะลุ
+            Damage_Percent = 0         -- ดาเมจที่จะทะลุ 
+        }
+	},
+    {
+		Name = `WEAPON_BOTTLELV3`,     -- ชื่ออาวุธ
+		Damage = 0.90,                 -- ความแรง
+        ArmourDamage = 25,             -- ดาเมจตายตัว เช่น 12 ก็คือ 12
+        Critral = 0,                   -- % คริ
+        Painkiller = {
+            Percent = 0,               -- โอกาสติดห้ามปั้มยา
+            TimeOut = 0,               -- สถานะคงอยู่กี่วินาที
+        },
+        Piercing = {
+            Percent = 0,               -- โอกาสทะลุ
+            Damage_Percent = 0         -- ดาเมจที่จะทะลุ 
+        }
+	},
+    {
+		Name = `WEAPON_BOTTLELV4`,     -- ชื่ออาวุธ
+		Damage = 0.90,                 -- ความแรง
+        ArmourDamage = 30,             -- ดาเมจตายตัว เช่น 12 ก็คือ 12
+        Critral = 0,                   -- % คริ
+        Painkiller = {
+            Percent = 0,               -- โอกาสติดห้ามปั้มยา
+            TimeOut = 0,               -- สถานะคงอยู่กี่วินาที
+        },
+        Piercing = {
+            Percent = 0,               -- โอกาสทะลุ
+            Damage_Percent = 0         -- ดาเมจที่จะทะลุ 
+        }
+	},
+    {
+		Name = `WEAPON_BOTTLELV5`,     -- ชื่ออาวุธ
+		Damage = 0.90,                 -- ความแรง
+        ArmourDamage = 35,             -- ดาเมจตายตัว เช่น 12 ก็คือ 12
+        Critral = 0,                   -- % คริ
+        Painkiller = {
+            Percent = 0,               -- โอกาสติดห้ามปั้มยา
+            TimeOut = 0,               -- สถานะคงอยู่กี่วินาที
+        },
+        Piercing = {
+            Percent = 0,               -- โอกาสทะลุ
+            Damage_Percent = 0         -- ดาเมจที่จะทะลุ 
+        }
+	},
+    {
+		Name = `WEAPON_POOLCUELV1`,    -- ชื่ออาวุธ
+		Damage = 0.23,                 -- ความแรง
+        ArmourDamage = 18,             -- ดาเมจตายตัว เช่น 12 ก็คือ 12
+        Critral = 0,                  -- % คริ
+        Painkiller = {
+            Percent = 100,               -- โอกาสติดห้ามปั้มยา
+            TimeOut = 5,               -- สถานะคงอยู่กี่วินาที
+        },
+        Piercing = {
+            Percent = 100,               -- โอกาสทะลุ
+            Damage_Percent = 10         -- ดาเมจที่จะทะลุ 
+        }
+	},
+    {
+		Name = `WEAPON_POOLCUELV2`,    -- ชื่ออาวุธ
+		Damage = 0.90,                 -- ความแรง
+        ArmourDamage = 20,             -- ดาเมจตายตัว เช่น 12 ก็คือ 12
+        Critral = 20,                  -- % คริ
+        Painkiller = {
+            Percent = 0,               -- โอกาสติดห้ามปั้มยา
+            TimeOut = 0,               -- สถานะคงอยู่กี่วินาที
+        },
+        Piercing = {
+            Percent = 0,               -- โอกาสทะลุ
+            Damage_Percent = 0         -- ดาเมจที่จะทะลุ 
+        }
+	},
+    {
+		Name = `WEAPON_POOLCUELV3`,    -- ชื่ออาวุธ
+		Damage = 0.90,                 -- ความแรง
+        ArmourDamage = 23,             -- ดาเมจตายตัว เช่น 12 ก็คือ 12
+        Critral = 50,                  -- % คริ
+        Painkiller = {
+            Percent = 0,               -- โอกาสติดห้ามปั้มยา
+            TimeOut = 0,               -- สถานะคงอยู่กี่วินาที
+        },
+        Piercing = {
+            Percent = 0,               -- โอกาสทะลุ
+            Damage_Percent = 0         -- ดาเมจที่จะทะลุ 
+        }
+	},
+    {
+		Name = `WEAPON_POOLCUELV4`,    -- ชื่ออาวุธ
+		Damage = 0.90,                 -- ความแรง
+        ArmourDamage = 25,             -- ดาเมจตายตัว เช่น 12 ก็คือ 12
+        Critral = 80,                  -- % คริ
+        Painkiller = {
+            Percent = 0,               -- โอกาสติดห้ามปั้มยา
+            TimeOut = 0,               -- สถานะคงอยู่กี่วินาที
+        },
+        Piercing = {
+            Percent = 0,               -- โอกาสทะลุ
+            Damage_Percent = 0         -- ดาเมจที่จะทะลุ 
+        }
+	},
+    {
+		Name = `WEAPON_POOLCUELV5`,    -- ชื่ออาวุธ
+		Damage = 0.90,                 -- ความแรง
+        ArmourDamage = 30,             -- ดาเมจตายตัว เช่น 12 ก็คือ 12
+        Critral = 100,                 -- % คริ
+        Painkiller = {
+            Percent = 0,               -- โอกาสติดห้ามปั้มยา
+            TimeOut = 0,               -- สถานะคงอยู่กี่วินาที
+        },
+        Piercing = {
+            Percent = 0,               -- โอกาสทะลุ
+            Damage_Percent = 0         -- ดาเมจที่จะทะลุ 
+        }
+	},
+}
